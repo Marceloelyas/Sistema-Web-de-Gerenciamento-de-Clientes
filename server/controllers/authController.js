1573d54a54010f5ef234ef488be21ac5cc47d273
Marcelo Elyas <69271760+Marceloelyas@users.noreply.github.com>
2025-07-16 19:57:44 -0300

    Create authController.js
    Add authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,54 @@
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+
+// @desc    Register user
+// @route   POST /api/auth/register
+// @access  Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Login user
+// @route   POST /api/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
+// Helper to send token response
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+
+  res.status(statusCode).json({
+    success: true,
+    token
+  });
+};
